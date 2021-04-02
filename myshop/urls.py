081608d12cd85a6e492825e18e6d16102a07262c
Ivanshamir <shamirimtiaz.ice@gmail.com>
2021-04-02 15:12:56 +0600

    save order into db

--- myshop/urls.py
+++ myshop/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('cart/', include('cart.urls', namespace='cart')),
     path('', include('shop.urls', namespace='shop')),
+    path('orders/', include('orders.urls', namespace='orders')),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
