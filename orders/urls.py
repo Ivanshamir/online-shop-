0b4a8f26c4d17939734906a8ed964e9902d68774
Ivanshamir <shamirimtiaz.ice@gmail.com>
2021-04-03 21:41:18 +0600

    csv export and custom detail page in admin

--- orders/urls.py
+++ orders/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'orders'
 
 urlpatterns = [
-    path('create/', views.order_create, name="order_create")
+    path('create/', views.order_create, name="order_create"),
+    path('admin/order/<int:order_id>/', views.admin_order_detail, name="admin_order_detail")
 ]
--- orders/views.py
+++ orders/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .forms import OrderCreateForm
-from .models import OrderItem
+from .models import OrderItem, Order
 from cart.cart import Cart
 from .tasks import order_created
 from django.urls import reverse
+from django.contrib.admin.views.decorators import staff_member_required
 
 
 # Create your views here.
@@ -33,3 +34,11 @@ def order_create(request):
     return render(request,
                   'orders/order/create.html',
                   {'cart': cart, 'form': form})
+
+
+@staff_member_required
+def admin_order_detail(request, order_id):
+    order = get_object_or_404(Order, id=order_id)
+    return render(request,
+                  'admin/orders/order/detail.html',
+                  {'order': order})
