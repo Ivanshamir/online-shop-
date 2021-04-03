fb1797bda7cb55daa30f208f9e8e4bfe6659a11f
Ivanshamir <shamirimtiaz.ice@gmail.com>
2021-04-03 22:26:21 +0600

    send pdf via email

--- myshop/settings.py
+++ myshop/settings.py
@@ -136,3 +136,5 @@ BRAINTREE_CONF = braintree.Configuration(
     BRAINTREE_PUBLIC_KEY,
     BRAINTREE_PRIVATE_KEY
 )
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
--- payment/views.py
+++ payment/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 import braintree
 from django.conf import settings
 from orders.models import Order
+from .tasks import payment_completed
 
 # Create your views here.
 gateway = braintree.BraintreeGateway(settings.BRAINTREE_CONF)
@@ -29,6 +30,8 @@ def payment_process(request):
             # store the unique transaction id
             order.braintree_id = result.transaction.id
             order.save()
+            # launch asynchronous task
+            payment_completed.delay(order.id)
             return redirect('payment:done')
         else:
             return redirect('payment:canceled')
